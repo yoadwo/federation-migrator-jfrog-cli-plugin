0a976922f85f9d817fe73978f5c6e1e555583178
romk <romk@jfrog.com>
2025-06-09 11:21:30 +0300

    RTFS-2079 - Add new options to call the migration tool for better configurability

---
+++ flags/flags.go
@@ -0,0 +1,17 @@
+package flags
+
+const (
+	Force                      = "force"
+	Parallel                   = "parallel"
+	BatchSize                  = "batch-size"
+	StatefulRun                = "stateful-run"
+	RtfsLegacyContextPath      = "rtfs-legacy-context-path"
+	HttpSocketTimeoutMs        = "http-socket-timeout-ms"
+	HttpMaxTotalConnections    = "http-max-total-connections"
+	HttpMaxConnectionsPerRoute = "http-max-connections-per-route"
+	HttpConnectionPoolTtlSec   = "http-connection-pool-ttl-sec"
+	HttpRetryCount             = "http-retry-count"
+	HttpVerboseMode            = "http-verbose-mode"
+	ExecutorTimeoutMin         = "executor-timeout-min"
+	ExecutorThreads            = "executor-threads"
+)
